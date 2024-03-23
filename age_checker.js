4ae514d070d0b8e897236c7882e72583c0ddbc99
oladmejialabim <oladimejialabim@gmail.com>
2024-03-23 23:40:39 +0100

    the page now set up with logo, input

---
+++ age_checker.js
@@ -0,0 +1,27 @@
+function ageChecker(){
+	// age_checker function
+	var userAge = parseInt(document.getElementById('ageInput').value);
+	var remainYears = 35 - userAge;
+
+	if (userAge < 18){
+		document.getElementById('result').innerHTML = "Sorry kid you have: " + remainYears + "years left to join this platform";
+	}else if (userAge >= 18 && userAge < 35 ){
+		document.getElementById('result').innerHTML = "Well, you can still be given access considering that you're an adolescent/Young Adult";
+		// console.log userAge;
+	}else if (userAge >= 35 && userAge < 45){
+		document.getElementById('result').innerHTML = "You so much welcome here";
+	}else if (userAge >= 45){
+		document.getElementById('result').innerHTML = "Hey! Platinum User, you are most welcome";
+		// console.log userAge;
+	}
+}
+
+// adding event listener that will clear the output once the input is cleared
+
+document.getElementById('ageInput').addEventListener('input', function(){
+	const input = this.value.trim(); // this will remove the reults
+	if (input === ''){
+		document.getElementById('result').innerHTML = '';
+	}
+} );
+
---
+++ maket_search.js
@@ -0,0 +1,13 @@
+// // this is mean to search the markets for what client wants
+//  const marketSearcher;
+//  switch (marketSearcher){
+//  	case "Lagos";
+//  	console.log("lagos");
+//  	document.getElementById('Lagos').innerHTML = 
+//  }
+
+// Language preference selction
+
+fuction langSelect (){
+	var English = document.getElementByTagName
+}
